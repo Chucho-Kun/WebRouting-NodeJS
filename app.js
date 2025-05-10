9c15c01f881e7a8dbd70eb7ea8992f1c8e7cd793
Jesus Peralta <jesus_web_master@hotmail.com>
2025-05-10 15:57:05 -0600

    aplicacion funcional con error MIME Type

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import path from 'path';
+import hbs from 'hbs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -8,22 +9,25 @@ const __dirname = dirname(__filename);
 import express from 'express';
 const app = express();
 
-app.use('/assets', express.static(path.join(__dirname, '/assets')));
+// Handlebars
+hbs.registerPartials(__dirname+'/views/partials', function(err){});
 
 app.set('view engine','hbs');
+app.use('/assets', express.static(path.join(__dirname, '/assets')));
+
+const nombre = 'Jesus Peralta';
+const titulo = 'ServiceWeb-NodeJS';
 
 app.get('/', (req, res) => {
-    res.render('home');
-  })
-
-//app.use( express.static( 'public' ) );
+    res.render('home',{ nombre , titulo });
+  });
 
   app.get('/generic', (req, res) => {
-    res.sendFile( __dirname + '/public/generic.html' );
+    res.render('generic',{ nombre , titulo });
   })
 
   app.get('/elements', (req, res) => {
-    res.sendFile( __dirname + '/public/elements.html' );
+    res.render('elements',{ nombre , titulo });
   })
 
   app.get('*', (req, res) => {
