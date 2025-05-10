05eab838d88e5ad6dd5b70489aa915d47532bf5e
Jesus Peralta <jesus_web_master@hotmail.com>
2025-05-10 15:01:10 -0600

    conflicto con MIME Type

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
+import path from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-import express from 'express'
-const app = express()
+import express from 'express';
+const app = express();
 
-app.use( express.static( 'public' ) );
+app.use('/assets', express.static(path.join(__dirname, '/assets')));
 
+app.set('view engine','hbs');
 
+app.get('/', (req, res) => {
+    res.render('home');
+  })
+
+//app.use( express.static( 'public' ) );
 
   app.get('/generic', (req, res) => {
     res.sendFile( __dirname + '/public/generic.html' );
